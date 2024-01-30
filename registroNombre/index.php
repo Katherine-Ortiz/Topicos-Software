28c810d9592373f13479affbb22553a6d20ba631
Katherine-Ortiz <74745343+Katherine-Ortiz@users.noreply.github.com>
2024-01-29 22:22:22 -0500

    Update index.php

--- registroNombre/index.php
+++ registroNombre/index.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>registroNombre</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
+
 <body>
     
 <form method="post">
 
-    <h1>Registrarse</h1>
-    <p>ingrese su nombre</p>
+    <h1>Registro</h1>
+    <p>Ingrese su nombre</p>
 
     <div class="input-wrapper">
         <input type="text" name="name" placeholder="Nombre">
